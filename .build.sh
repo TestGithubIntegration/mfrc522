a736f860deee5887b34fc9bd32bb4ce673d39e88
TestGithubIntegration <34926945+TestGithubIntegration@users.noreply.github.com>
2018-08-25 00:32:02 +0200

    Update README.rst (#1)

---
+++ .build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -ev # fail fast and hard
+
+echo "Test example: $PLATFORMIO_CI_SRC";
+
+board_compile_param=""; # global variable
+
+stringContain() { [ -z "${2##*$1*}" ]; }
+buildBoardParam() {
+        if stringContain "$2" "$1";
+                then echo "check board $board";
+                board_compile_param="$board_compile_param $3";
+        else
+                echo "skip board test of $board";
+        fi        
+}
+
+board="arduino_avr";
+boardparam="--board=uno --board=megaatmega1280";
+buildBoardParam "$TESTBOARD" "$board" "$boardparam"
+
+board="arduino_arm";
+boardparam="--board=due --board=zero";
+buildBoardParam "$TESTBOARD" "$board" "$boardparam"
+
+board="teensy";
+boardparam="--board=teensy20 --board=teensy31"; 
+buildBoardParam "$TESTBOARD" "$board" "$boardparam"
+
+board="esp";
+boardparam="--board=d1_mini";
+buildBoardParam "$TESTBOARD" "$board" "$boardparam"
+
+## build
+output=$(platformio ci -v --lib=. $board_compile_param 2>&1 >/dev/null  | sed "s/\/tmp\/[a-zA-Z0-9_]*\/src\/[a-zA-Z0-9_]*\.ino/${PLATFORMIO_CI_SRC//\//\\/}/g" | sed 's/lib\/[a-zA-Z0-9_]*\///g');
+echo "--<Result>--";
+echo "$output" | reviewdog -name="compiler" -efm="%f:%l:%c: %m" -diff="git diff master" -reporter=github-pr-check;
+echo "$output";
+echo "$output" | grep -i -E "^(Device|Data|Program|text|[0-9])";
