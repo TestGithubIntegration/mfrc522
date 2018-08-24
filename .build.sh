8891c709f8d63b6eb857681efb551c53210f6beb
TestGithubIntegration <34926945+TestGithubIntegration@users.noreply.github.com>
2018-08-24 13:56:51 +0200

    Create .build.sh

---
+++ .build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+board_compile_param=""; # global variable
+
+stringContain() { [ -z "${2##*$1*}" ]; }
+buildBoardParam() {
+        if stringContain "$2" "$1";
+                then echo "check board $board";
+                board_compile_param="$board_compile_param $3";
+        else
+                echo "skip board test of $board";
+        fi        
+}
+
+board="arduino_avr";
+boardparam="--board=uno --board=megaatmega1280";
+buildBoardParam "$TESTBOARD" "$board" "$boardparam"
+
+board="arduino_arm";
+boardparam="--board=due --board=zero";
+buildBoardParam "$TESTBOARD" "$board" "$boardparam"
+
+board="teensy";
+boardparam="--board=teensy20 --board=teensy31"; 
+buildBoardParam "$TESTBOARD" "$board" "$boardparam"
+
+board="esp";
+boardparam="--board=d1_mini";
+buildBoardParam "$TESTBOARD" "$board" "$boardparam"
+
+## build
+output=$(platformio ci -v --lib=. "$boardparam");
+echo "--<Result>--";
+echo "$output" | grep -i -E "^(Device|Data|Program|text|[0-9])";
