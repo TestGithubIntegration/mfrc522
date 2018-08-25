94bfba404cc106de06bac8c862c422cdfe029160
TestGithubIntegration <34926945+TestGithubIntegration@users.noreply.github.com>
2018-08-25 20:46:08 +0200

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -84,16 +84,3 @@ buildBoardParam "$TESTBOARD" "$board" "${boardparam[@]}"
 board='esp';
 boardparam=('d1_mini');
 buildBoardParam "$TESTBOARD" "$board" "${boardparam[@]}"
-
-
-${CACHED}/${NAMEVER}/cppcheck --version
-cppcheck_example=$(${CACHED}/${NAMEVER}/cppcheck --language=c++ --std=c++11 --enable=warning,style,performance,portability --template='{file}:{line}:{column}: {severity}: {message} {callstack}' $(find examples/ -regextype posix-extended -regex ".*\.(h|c|hpp|cpp|ino)") 2>&1 1>/dev/null);
-echo "$cppcheck_example";
-echo "$cppcheck_example" | reviewdog -name="cppcheck_examples" -efm="%f:%l:%c: %m" -reporter=github-pr-check;
-
-cppcheck_source=$(${CACHED}/${NAMEVER}/cppcheck --language=c++ --std=c++11 --enable=warning,style,performance,portability --template='{file}:{line}:{column}: {severity}: {message} {callstack}' $(find src/ -regextype posix-extended -regex ".*\.(h|c|hpp|cpp)") 2>&1 1>/dev/null);
-echo "$cppcheck_source";
-echo "$cppcheck_source" | reviewdog -name="cppcheck_source" -efm="%f:%l:%c: %m" -reporter=github-pr-check;
-
-
-find src/ -regextype posix-extended -regex ".*\.(h|c|hpp|cpp)";
