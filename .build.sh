9dd7762b5d8db7f20add1923b89cfc21cf022b00
TestGithubIntegration <34926945+TestGithubIntegration@users.noreply.github.com>
2018-08-25 03:16:26 +0200

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -31,11 +31,23 @@ buildMe(){
 #        echo "$output_reviewdog" | reviewdog -name="build_$board" -efm="%f:%l:%c: %m" -diff="git diff" -reporter=github-pr-check;
         echo "$output_reviewdog" | reviewdog -name="build_$board" -efm="%f:%l:%c: %m" -reporter=github-pr-check;
         echo '###############';
-        echo "$output_reviewdog";
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+        echo -e "${Red}$output_reviewdog";
         echo '###############';
-        echo "$output_unfiltered";
+        #echo "$output_unfiltered";
         echo '###############';
-        echo "$output_unfiltered" | grep -i -E "^(Device|Data|Program|text|[0-9])";
+        echo -e "${Cyan}$output_unfiltered" | grep -i -E "^(Device|Data|Program|text|[0-9])";
 }
 
 
