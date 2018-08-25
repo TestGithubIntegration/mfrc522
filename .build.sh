071000bfe529d53d33f51a3203b8f86245167cfc
TestGithubIntegration <34926945+TestGithubIntegration@users.noreply.github.com>
2018-08-25 02:39:23 +0200

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -4,39 +4,13 @@ set -v # fail fast and hard
 
 echo "Test example: $PLATFORMIO_CI_SRC";
 
-board_compile_param=""; # global variable
-
 stringContain() { [ -z "${2##*$1*}" ]; }
-buildBoardParam() {
-        if stringContain "$2" "$1";
-                then echo "check board $board";
-                board_compile_param="$board_compile_param $3";
-        else
-                echo "skip board test of $board";
-        fi        
-}
-
-board="arduino_avr";
-boardparam="--board=uno --board=megaatmega1280";
-buildBoardParam "$TESTBOARD" "$board" "$boardparam"
-
-board="arduino_arm";
-boardparam="--board=due --board=zero";
-buildBoardParam "$TESTBOARD" "$board" "$boardparam"
-
-board="teensy";
-boardparam="--board=teensy20 --board=teensy31"; 
-buildBoardParam "$TESTBOARD" "$board" "$boardparam"
-
-board="esp";
-boardparam="--board=d1_mini";
-buildBoardParam "$TESTBOARD" "$board" "$boardparam"
-
 
 buildMe(){
         board=$1;
         echo '***build***';
         echo "board: $board";
+        echo '***********';
         
         TMP=$(mktemp);
         var=$(platformio ci -v --lib=. --board="$board" 2> "$TMP");
@@ -63,4 +37,34 @@ buildMe(){
         echo "$output_unfiltered" | grep -i -E "^(Device|Data|Program|text|[0-9])";
 }
 
-buildMe 'uno';
+
+buildBoardParam() {
+        if stringContain "$2" "$1";
+                then echo "check board group $board";
+                arr=( "$@" )
+                for i in "${arr[@]}";
+                        do
+                                echo "Build board: $i"
+                                buildMe "$i"
+                        done
+        else
+                echo "skip board test of $board";
+        fi        
+}
+
+
+board="arduino_avr";
+boardparam=("uno" "megaatmega1280");
+buildBoardParam "$TESTBOARD" "$board" "${boardparam[@]}"
+
+board="arduino_arm";
+boardparam=("due" "zero");
+buildBoardParam "$TESTBOARD" "$board" "${boardparam[@]}"
+
+board="teensy";
+boardparam=("teensy20" "teensy31"); 
+buildBoardParam "$TESTBOARD" "$board" "${boardparam[@]}"
+
+board="esp";
+boardparam=("d1_mini");
+buildBoardParam "$TESTBOARD" "$board" "${boardparam[@]}"
