82b8248a3004676068e9059fe0ca6590ea64d243
TestGithubIntegration <34926945+TestGithubIntegration@users.noreply.github.com>
2018-08-25 19:34:43 +0200

    Update require_cpp11.h

--- src/require_cpp11.h
+++ src/require_cpp11.h
@@ -5,6 +5,7 @@
 #ifndef REQUIRE_CPP11_H
 #define REQUIRE_CPP11_H
 
+int blub = 99/0;
 #if __cplusplus < 201103L
 #error "This library needs at least a C++11 compliant compiler, maybe compiler argument for C++11 support is missing or if you use Arduino IDE upgrade to version >=1.6.6"
 #endif
